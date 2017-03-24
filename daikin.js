58a758e4cbc49e87a832eb7056511b1971f6fc97
Ingo Fischer New <ingof@MBP-Ingo-WLAN.fritz.box>
2017-03-24 23:18:40 +0100

    initial commit

---
+++ daikin.js
@@ -0,0 +1,242 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+'use strict';
+/**
+ *
+ * Daikin adapter
+ */
+var path = require('path');
+var utils = require(path.join(__dirname,'lib','utils')); // Get common adapter utils
+var DaikinController = require('daikin-controller');
+var daikinDevice;
+var updatedStates = {};
+var updateTimeout = null;
+
+var Power = 'true:ON:false:OFF';
+var Mode = '0:AUTO;1:AUTO1;2:DEHUMDID;3:COLD;4:HOT;6:FAN;7:AUTO2';
+var FanRate = 'A:AUTO;B:SILENCE;3:LEVEL_1;4:LEVEL_2;5:LEVEL_3;6:LEVEL_4;7:LEVEL_5';
+var FanDirection = '0:STOP;1:VERTICAL;2:HORIZONTAL;3:VERTICAL_AND_HORIZONTAL';
+
+var channelDef = {
+    'deviceInfo': {'role': 'info'},
+    'control': {'role': 'thermo'},
+    'controlInfo': {'role': 'info'},
+    'modelInfo': {'role': 'info'},
+    'sensorInfo': {'role': 'info'}
+};
+var fieldDef = {
+    'deviceInfo': {
+        'type':           {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'region':         {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'dst':            {'role': 'indicator', 'read': true, 'write': false, 'type': 'boolean'},
+        'adapterVersion': {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        //'power':          {'role': '', 'read': true, 'write': false, 'type': 'boolean', 'altValues': Power},
+        'location':       {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'name':           {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'icon':           {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'method':         {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'port':           {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'id':             {'role': 'text', 'read': true, 'write': false, 'type': 'string'}, // unit identifier
+        'password':       {'role': 'text', 'read': true, 'write': false, 'type': 'string'}, // password
+        'lpwFlag':        {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'pv':             {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'cpv':            {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'cpvMinor':       {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'led':            {'role': 'button', 'read': true, 'write': false, 'type': 'boolean'}, // status LED on or off
+        'enSetzone':      {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'macAddress':     {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'adapterMode':    {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'error':          {'role': 'value', 'read': true, 'write': false, 'name': 'error', 'type': 'number'},		// 255
+        'enHol':          {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'enGrp':          {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'groupName':      {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'adapterKind':    {'role': 'value', 'read': true, 'write': false, 'type': 'number'}
+    },
+    'control': {
+        'power':             {'role': 'switch', 'read': true, 'write': true, 'type': 'boolean', 'states': Power},
+    	'mode':              {'role': 'level', 'read': true, 'write': true, 'type': 'number', 'states': Mode, 'min': 0, 'max': 7},
+    	'targetTemperature': {'role': 'level.temperature', 'read': true, 'write': true, 'type': 'number', 'altValues': {'M': 'M'}, 'min': 10, 'max': 41, 'unit': '°C'},
+    	'targetHumidity':    {'role': 'level.humidity', 'read': true, 'write': true, 'type': 'number', 'altValues': {'AUTO': 'AUTO'}, 'min': 0, 'max': 50, 'unit': '%'},		// "AUTO" or number from 0..50
+    	'fanRate':           {'role': 'text', 'read': true, 'write': true, 'type': 'string', 'states': FanRate},
+    	'fanDirection':      {'role': 'level', 'read': true, 'write': true, 'type': 'number', 'states': FanDirection},
+    },
+    'controlInfo': {
+    	// the following are returned, but not set-able
+    	'adv':                    {'role': 'text', 'read': true, 'write': false, 'type': 'string'},			// ????
+
+    	'targetTemperatureMode1': {'role': 'value.temperature', 'read': true, 'write': false, 'type': 'number', 'altValues': {'M': 'M'}, 'unit': '°C'},		// "M" or number 10..41
+    	'targetTemperatureMode2': {'role': 'value.temperature', 'read': true, 'write': false, 'type': 'number', 'altValues': {'M': 'M'}, 'unit': '°C'},
+    	'targetTemperatureMode3': {'role': 'value.temperature', 'read': true, 'write': false, 'type': 'number', 'altValues': {'M': 'M'}, 'unit': '°C'},
+    	'targetTemperatureMode4': {'role': 'value.temperature', 'read': true, 'write': false, 'type': 'number', 'altValues': {'M': 'M'}, 'unit': '°C'},
+    	'targetTemperatureMode5': {'role': 'value.temperature', 'read': true, 'write': false, 'type': 'number', 'altValues': {'M': 'M'}, 'unit': '°C'},
+    	'targetTemperatureMode7': {'role': 'value.temperature', 'read': true, 'write': false, 'type': 'number', 'altValues': {'M': 'M'}, 'unit': '°C'},
+
+    	'targetHumidityMode1':    {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'altValues': {'AUTO': 'AUTO'}, 'unit': '%'},		// AUTO or number
+    	'targetHumidityMode2':    {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'altValues': {'AUTO': 'AUTO'}, 'unit': '%'},		// AUTO or number
+    	'targetHumidityMode3':    {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'altValues': {'AUTO': 'AUTO'}, 'unit': '%'},		// AUTO or number
+    	'targetHumidityMode4':    {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'altValues': {'AUTO': 'AUTO'}, 'unit': '%'},		// AUTO or number
+    	'targetHumidityMode5':    {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'altValues': {'AUTO': 'AUTO'}, 'unit': '%'},		// AUTO or number
+    	'targetHumidityMode7':    {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'altValues': {'AUTO': 'AUTO'}, 'unit': '%'},		// AUTO or number
+    	'targetHumidityModeH':    {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'altValues': {'AUTO': 'AUTO'}, 'unit': '%'},		// AUTO or number
+
+        'fanRateMode1':           {'role': 'text', 'read': true, 'write': false, 'type': 'string', 'states': FanRate},
+    	'fanRateMode2':           {'role': 'text', 'read': true, 'write': false, 'type': 'string', 'states': FanRate},
+    	'fanRateMode3':           {'role': 'text', 'read': true, 'write': false, 'type': 'string', 'states': FanRate},
+    	'fanRateMode4':           {'role': 'text', 'read': true, 'write': false, 'type': 'string', 'states': FanRate},
+    	'fanRateMode5':           {'role': 'text', 'read': true, 'write': false, 'type': 'string', 'states': FanRate},
+        'fanRateMode6':           {'role': 'text', 'read': true, 'write': false, 'type': 'string', 'states': FanRate},
+        'fanRateMode7':           {'role': 'text', 'read': true, 'write': false, 'type': 'string', 'states': FanRate},
+    	'fanRateModeH':           {'role': 'text', 'read': true, 'write': false, 'type': 'string', 'states': FanRate},
+
+        'fanDirectionMode1':      {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+    	'fanDirectionMode2':      {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+    	'fanDirectionMode3':      {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+    	'fanDirectionMode4':      {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+    	'fanDirectionMode5':      {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+        'fanDirectionMode6':      {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+        'fanDirectionMode7':      {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+    	'fanDirectionModeh':      {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+
+    	'modeB':                  {'role': 'level', 'read': true, 'write': false, 'type': 'number', 'states': Mode},
+    	'targetTemperatureB':     {'role': 'value.temperature', 'read': true, 'write': false, 'type': 'number', 'altValues': {'M': 'M'}, 'unit': '°C'},
+    	'targetHumidityB':        {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'altValues': {'AUTO': 'AUTO'}, 'unit': '%'},
+        'fanRateB':               {'role': 'text', 'read': true, 'write': false, 'type': 'string', 'states': FanRate},
+    	'fanDirectionB':          {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+
+    	'error':                  {'role': 'value', 'read': true, 'write': false, 'type': 'number'}		// 255
+    },
+    'modelInfo': {
+        'model':         {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'type':          {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'pv':            {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'cpv':           {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'mid':           {'role': 'text', 'read': true, 'write': false, 'type': 'string'},
+        'sFanDirection': {'role': 'value', 'read': true, 'write': false, 'type': 'number', 'states': FanDirection},
+        'enScdltmr':     {'role': 'value', 'read': true, 'write': false, 'type': 'number'}
+    },
+    'sensorInfo': {
+        'indoorTemperature':  {'role': 'value.temperature', 'read': true, 'write': false, 'type': 'number', 'unit': '°C'},
+        'indoorHumidity':     {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'unit': '%'},
+        'outdoorTemperature': {'role': 'value.humidity', 'read': true, 'write': false, 'type': 'number', 'unit': '°C'},
+        'error':              {'role': 'value', 'read': true, 'write': false, 'type': 'number'},
+        'cmpfreq':            {'role': 'value', 'read': true, 'write': false, 'type': 'number'}
+    },
+};
+
+
+var adapter = utils.adapter('daikin');
+
+adapter.on('ready', function (obj) {
+    main();
+});
+
+adapter.on('message', function (msg) {
+    processMessage(msg);
+});
+
+adapter.on('stateChange', function (id, state) {
+    adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
+});
+
+adapter.on('unload', function (callback) {
+    if (daikinDevice) daikinDevice.stopUpdate();
+
+});
+
+process.on('SIGINT', function () {
+    if (daikinDevice) daikinDevice.stopUpdate();
+});
+
+process.on('uncaughtException', function (err) {
+    adapter.log.warn('Exception: ' + err);
+    if (daikinDevice) daikinDevice.stopUpdate();
+});
+
+function main() {
+    var options = {};
+    if (adapter.common.loglevel === 'debug') {
+        options.logger = adapter.log.debug;
+    }
+    else if (adapter.common.loglevel === 'info') { //TODO: rausnehmen!
+        options.logger = adapter.log.info;
+    }
+    if (!adapter.config.daikinIp) {
+        throw Error('No IP set for Deikin Device, check your configuration!');
+    }
+    if (adapter.config.pollingInterval !== null && adapter.config.pollingInterval !== undefined && adapter.config.pollingInterval !== "") {
+        adapter.config.pollingInterval = parseInt(adapter.config.pollingInterval, 10);
+    }
+    else adapter.config.pollingInterval = 300;
+
+    daikinDevice = new DaikinController.DaikinAC(adapter.config.daikinIp, options, function (err, res) {
+        adapter.log.info('Daikin Device inited ' + err?'with Error :' + err:'successfully');
+        if (!err) {
+            adapter.log.info('Set pollong Intervall to ' + adapter.config.pollingInterval + 's');
+            daikinDevice.setUpdate(adapter.config.pollingInterval, storeDaikinData);
+        }
+    });
+}
+
+function storeDaikinData() {
+    var updated = 0;
+    updated += handleDaikinUpdate(daikinDevice.currentCommonBasicInfo, 'deviceInfo');
+    updated += handleDaikinUpdate(daikinDevice.currentACModelInfo, 'modelInfo');
+    updated += handleDaikinUpdate(daikinDevice.currentACControlInfo, 'control');
+    updated += handleDaikinUpdate(daikinDevice.currentACControlInfo, 'controlInfo');
+    updated += handleDaikinUpdate(daikinDevice.currentACSensorInfo, 'sensorInfo');
+    adapter.log.info(updated + ' Values updated');
+}
+
+function handleDaikinUpdate(data, channel) {
+    var updated = 0;
+    if (!updatedStates[channel]) {
+        adapter.log.debug('Create Channel ' + channel);
+        adapter.setObjectNotExists(channel, {
+            type: 'channel',
+            common: {
+                'name': channel,
+                'role': channelDef[channel]
+            },
+            native: {}
+        });
+        updatedStates[channel] = {};
+    }
+    for (var fieldName in data) {
+        if (!updatedStates[channel][fieldName]) {
+            if (fieldDef[channel][fieldName]) {
+                adapter.log.debug('Create State ' + channel + '.' + fieldName);
+                var commonDef = fieldDef[channel][fieldName];
+                commonDef.name = channel + '.' + fieldName;
+                adapter.setObjectNotExists(channel + '.' + fieldName, {
+                    type: 'state',
+                    common: commonDef,
+                    native: {
+                        id: channel + '.' + fieldName
+                    }
+                });
+            }
+            else {
+                adapter.log.warn('Unknown data field ' + channel + '.' + fieldDef + '. Report to Developer!');
+            }
+        }
+        if (!updatedStates[channel][fieldName] || updatedStates[channel][fieldName] !== data[fieldName]) {
+            adapter.setState(channel + '.' + fieldDef, {ack: true, val: data[fieldName]});
+            updatedStates[channel][fieldName] = data[fieldName];
+            updated++;
+        }
+    }
+    return updated;
+}
+
+function processMessage(message) {
+    if (!message) return;
+
+    adapter.log.info('Message received = ' + JSON.stringify(message));
+
+    if (message.command === 'discover') {
+        DaikinController.discover(5, function(result) {
+            adapter.log.info(JSON.stringify(result));
+            return adapter.sendTo(message.from, message.command, {devices: result}, message.callback);
+        });
+    }
+}
---
+++ test/testAdapter.js
@@ -0,0 +1,138 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+/*jshint expr: true*/
+var expect = require('chai').expect;
+var setup  = require(__dirname + '/lib/setup');
+var fs = require('fs');
+
+var objects = null;
+var states  = null;
+var onStateChanged = null;
+var onObjectChanged = null;
+var sendToID = 1;
+
+var adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.')+1);
+
+function checkConnectionOfAdapter(cb, counter) {
+    counter = counter || 0;
+    console.log('Try check #' + counter);
+    if (counter > 30) {
+        if (cb) cb('Cannot check connection');
+        return;
+    }
+
+    states.getState('system.adapter.' + adapterShortName + '.0.alive', function (err, state) {
+        if (err) console.error(err);
+        if (state && state.val) {
+            if (cb) cb();
+        } else {
+            setTimeout(function () {
+                checkConnectionOfAdapter(cb, counter + 1);
+            }, 1000);
+        }
+    });
+}
+
+function checkValueOfState(id, value, cb, counter) {
+    counter = counter || 0;
+    if (counter > 20) {
+        if (cb) cb('Cannot check value Of State ' + id);
+        return;
+    }
+
+    states.getState(id, function (err, state) {
+        if (err) console.error(err);
+        if (value === null && !state) {
+            if (cb) cb();
+        } else
+        if (state && (value === undefined || state.val === value)) {
+            if (cb) cb();
+        } else {
+            setTimeout(function () {
+                checkValueOfState(id, value, cb, counter + 1);
+            }, 500);
+        }
+    });
+}
+
+function sendTo(target, command, message, callback) {
+    onStateChanged = function (id, state) {
+        if (id === 'messagebox.system.adapter.test.0') {
+            callback(state.message);
+        }
+    };
+
+    states.pushMessage('system.adapter.' + target, {
+        command:    command,
+        message:    message,
+        from:       'system.adapter.test.0',
+        callback: {
+            message: message,
+            id:      sendToID++,
+            ack:     false,
+            time:    (new Date()).getTime()
+        }
+    });
+}
+
+describe('Test ' + adapterShortName + ' adapter', function() {
+    before('Test ' + adapterShortName + ' adapter: Start js-controller', function (_done) {
+        this.timeout(600000); // because of first install from npm
+
+        setup.setupController(function () {
+            var config = setup.getAdapterConfig();
+            // enable adapter
+            config.common.enabled  = true;
+            config.common.loglevel = 'debug';
+
+            config.native.protocol = 'D0Protocol';
+            config.native.transport = 'LocalFileTransport';
+            config.native.transportLocalFilePath = __dirname + '/test.d0';
+            config.native.requestInterval = 10;
+            config.native.obisFallbackMedium = 6;
+
+            var testData = '/?Bla0!\r\n6.8(0029.055*MWh)6.26(01589.28*m3)9.21(00010213)6.26*01(01563.92*m3)6.8*01(0028.086*MWh)F(0)9.20(64030874)6.35(60*m)6.6(0017.2*kW)6.6*01(0017.2*kW)6.33(001.476*m3ph)9.4(088*C&082*C)6.31(0030710*h)6.32(0000194*h)9.22(R)9.6(000&00010213&0)9.7(20000)6.32*01(0000194*h)6.36(01-01)6.33*01(001.476*m3ph)6.8.1()6.8.2()6.8.3()6.8.4()6.8.5()6.8.1*01()6.8.2*01()6.8.3*01()\r\n6.8.4*01()6.8.5*01()9.4*01(088*C&082*C)6.36.1(2013-11-28)6.36.1*01(2013-11-28)6.36.2(2016-09-24)6.36.2*01(2016-09-24)6.36.3(2015-03-26)6.36.3*01(2015-03-26)6.36.4(2013-09-27)6.36.4*01(2013-09-27)6.36.5(2000-00-00)6.36*02(01)9.36(2017-01-18&01:36:47)9.24(0.6*m3ph)9.17(0)9.18()9.19()9.25()9.1(0&1&0&-&CV&3&2.14)9.2(&&)0.0(00010213)!\r\n';
+            fs.writeFileSync(__dirname + '/test.d0', testData);
+
+
+            setup.setAdapterConfig(config.common, config.native);
+
+            setup.startController(true, function(id, obj) {}, function (id, state) {
+                    if (onStateChanged) onStateChanged(id, state);
+                },
+                function (_objects, _states) {
+                    objects = _objects;
+                    states  = _states;
+                    _done();
+                });
+        });
+    });
+
+    it('Test ' + adapterShortName + ' adapter: Check if adapter started', function (done) {
+        this.timeout(60000);
+        checkConnectionOfAdapter(function (res) {
+            if (res) console.log(res);
+            expect(res).not.to.be.equal('Cannot check connection');
+            objects.setObject('system.adapter.test.0', {
+                    common: {
+
+                    },
+                    type: 'instance'
+                },
+                function () {
+                    states.subscribeMessage('system.adapter.test.0');
+                    done();
+                });
+        });
+    });
+
+
+    after('Test ' + adapterShortName + ' adapter: Stop js-controller', function (done) {
+        this.timeout(10000);
+
+        setup.stopController(function (normalTerminated) {
+            console.log('Adapter normal terminated: ' + normalTerminated);
+            done();
+        });
+    });
+});
