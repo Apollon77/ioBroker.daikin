a7f81f84268e2a643aef12e081f1c550d354fe57
Ingo Fischer <github@fischer-ka.de>
2017-10-16 16:22:35 +0200

    update basic package-file testing

--- test/testPackageFiles.js
+++ test/testPackageFiles.js
@@ -1,5 +1,7 @@
-/* jshint -W097 */// jshint strict:false
-/*jslint node: true */
+/* jshint -W097 */
+/* jshint strict:false */
+/* jslint node: true */
+/* jshint expr: true */
 var expect = require('chai').expect;
 var fs        = require('fs');
 
