bcfb2f39c244c14faa65d0027cbf2b969b991b16
Ingo Fischer <github@fischer-ka.de>
2018-01-02 13:34:16 +0100

    translations

---
+++ admin/words.js
@@ -0,0 +1,11 @@
+systemDictionary = {
+    "page_title":   {"en": "Daikin Adapter Settings", "de": "Daikin Adapter Einstellungen"},
+
+    "heading_GeneralSettings":   {"en": "General Settings", "de": "Generelle Einstellungen"},
+
+    "field_daikinIp":   {"en": "IP of Daikin Device", "de": "IP des Daikin-Gerätes"},
+    "info_daikinIp":   {"en": " ", "de": " "},
+
+    "field_pollingInterval":   {"en": "Data update interval", "de": "Daten Aktualisierungsintervall"},
+    "info_pollingInterval":   {"en": " ", "de": " "}
+};
