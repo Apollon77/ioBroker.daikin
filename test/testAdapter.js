34c3d8caf60824b566ca99692d0fe652708d0c61
Ingo Fischer <github@fischer-ka.de>
2018-02-18 00:09:33 +0100

    fix testing

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -120,6 +120,10 @@ var responses = [
         'response': 'ret=OK,adv=12/13'
     },
     {
+        'request': 'GET /aircon/get_control_info',
+        'response': 'ret=OK,pow=0,mode=2,adv=12/13,stemp=25.0,shum=0,dt1=25.0,dt2=M,dt3=23.0,dt4=27.0,dt5=27.0,dt7=25.0,dh1=AUTO,dh2=50,dh3=0,dh4=0,dh5=0,dh7=AUTO,dhh=50,b_mode=3,b_stemp=23.0,b_shum=0,alert=255,f_rate=A,f_dir=0,b_f_rate=A,b_f_dir=0,dfr1=5,dfr2=5,dfr3=A,dfr4=5,dfr5=5,dfr6=5,dfr7=5,dfrh=5,dfd1=0,dfd2=0,dfd3=0,dfd4=0,dfd5=0,dfd6=0,dfd7=0,dfdh=0'
+    },
+    {
         'request': 'POST /aircon/set_control_info',
         'response': 'ret=OK,adv=12/13'
     },
